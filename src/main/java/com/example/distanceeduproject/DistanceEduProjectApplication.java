d8cd18632b8309040bfa600c9a2e50bf2d01a2df
Nazarkep355 <61923362+Nazarkep355@users.noreply.github.com>
2023-03-14 14:01:47 +0200

    Initial commit

---
+++ src/main/java/com/example/distanceeduproject/DistanceEduProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.distanceeduproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceEduProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceEduProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/distanceeduproject/DistanceEduProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distanceeduproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceEduProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
